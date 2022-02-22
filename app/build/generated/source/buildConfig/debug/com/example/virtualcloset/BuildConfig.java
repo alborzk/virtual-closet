092ffe92274c190c8f137d9d3f0186c9b613a0e6
alborzk <khakbaza@myumanitoba.ca>
2022-02-21 22:54:19 -0600

    Initial Setup

---
+++ app/build/generated/source/buildConfig/debug/com/example/virtualcloset/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.virtualcloset;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.virtualcloset";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
