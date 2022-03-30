2dbb773ab64310762172e88082e9e8bf7a6d7c83
Gyuri Kim <kimg2@myumanitoba.ca>
2022-03-30 10:15:27 +0900

    update OutfitDataManagerTest

--- app/src/test/java/com/example/virtualcloset/logic/OufitDataManagerTest.java
+++ app/src/test/java/com/example/virtualcloset/logic/OufitDataManagerTest.java
@@ -12,11 +12,39 @@ import java.util.ArrayList;
 public class OufitDataManagerTest {
     Database db = new Database();
     ArrayList<ClothesItem> clothesItems;
+    OutfitDataManager newDM = new OutfitDataManager(db);
+
+    @Test
+    public void testGetOutfitName() {
+        assertEquals(2, newDM.getOutfitName().length);
+        assertEquals("WORK", newDM.getOutfitName()[0]);
+        assertEquals("CASUAL", newDM.getOutfitName()[1]);
+    }
+
     @Test
     public void getID() {
-        OufitDataManager newDM = new OufitDataManager(db);
+        //OufitDataManager newDM = new OufitDataManager(db);
         assertEquals(2,newDM.getID().length);
         assertEquals(0,newDM.getID()[0]);
         assertEquals(1,newDM.getID()[1]);
+    }
+
+    @Test
+    public void testGetClothesList() {
+        assertEquals(2, newDM.getClothesList(0).length); // WORK
+        assertEquals(2, newDM.getClothesList(1).length); // CASUAL
+    }
+
+    @Test
+    public void testGetNames() {
+        assertEquals("Gymshark Joggers", newDM.getNames(db.clothesList1)[0]); // ?
+    }
+
+    @Test
+    public void testGetTags() {
+    }
+
+    @Test
+    public void testGetImgs() {
     }
 }
