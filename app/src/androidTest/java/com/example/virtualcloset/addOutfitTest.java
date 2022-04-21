0ed7fd1f2e241d0918a623b97c00ab66872ca457
zhou <zhoux5@myumanitoba>
2022-04-21 17:54:14 -0500

    addFlagTest,removeTagTest

--- app/src/androidTest/java/com/example/virtualcloset/addOutfitTest.java
+++ app/src/androidTest/java/com/example/virtualcloset/addOutfitTest.java
@@ -1,12 +1,15 @@
 package com.example.virtualcloset;
-
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static androidx.test.espresso.action.ViewActions.*;
+import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
 
+import androidx.test.espresso.Espresso;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
@@ -17,41 +20,24 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class viewClothesTest {
+public class addOutfitTest {
     @Rule
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
 
     @Before
     public void setupDB() {
         //here we will delete the account that will be created
+        //make sure we have Gymshark Joggers in our closet at position 0
+        //flag
+
         //login default account
         onView(withId(R.id.editTextTextEmailAddress)).perform(typeText("user"));
         onView(withId(R.id.editTextTextPassword)).perform(typeText("password"))
-                .perform(closeSoftKeyboard());
+                .perform(closeSoftKeyboard()); //close the keyboard after text input
         onView(withId(R.id.loginButton)).perform(click());
-        onView(withId(R.id.navigation_clothes)).perform(click());
+        onView(withId(R.id.navigation_outfits)).perform(click());
 
-        //make sure we have Gymshark Joggers in our closet
-    }
-
-    @Test
-    public void viewClothes() {
-        //check the first item on GridView
-        onData(anything()).inAdapterView(withId(R.id.gridView)).atPosition(0)
-                .onChildView(withId(R.id.item_name))
-                .check(matches(withText("Gymshark Joggers")));
-
-    }
-
-    @Test
-    public void viewClothesDetails() {
-        //click the first item on GridView
-        //check the details of item
-        onData(anything()).inAdapterView(withId(R.id.gridView)).atPosition(0).perform(click());
-        onView(withId(R.id.nameDisplay)).check(matches(withText("Gymshark Joggers")));
-        onView(withId(R.id.tagDisplay)).check(matches(withText("|  Black  |  Pants  |  Workout  |  ")));
     }
 }
